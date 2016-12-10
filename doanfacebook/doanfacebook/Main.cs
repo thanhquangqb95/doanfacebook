155d9102ed2cd8bd654108f43f8df4562dcb8d19
thanhquangqb95 <thanhquangqb95@gmail.com>
2016-12-11 00:30:07 +0700

    11/12/2016 s1

---
+++ doanfacebook/doanfacebook/Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace doanfacebook
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minDangNhap(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _Click(object sender, EventArgs e)
+        {
+            
+            frmDangNhap FrmDangNhap   = new frmDangNhap();
+            FrmDangNhap.ShowDialog();
+            this.Close();
+            
+        }
+    }
+}
---
+++ doanfacebook/doanfacebook/frmDangNhap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Facebook;
+using System.IO;
+
+namespace doanfacebook
+{
+    public partial class frmDangNhap : Form
+    {
+
+        public class TaiKhoan
+        {
+            public string ten { get; set; }
+            public string id { get; set; }
+            public string hinh { get; set; }
+            public TaiKhoan(string a, string b, string c)
+            {
+                ten = a;
+                id = b;
+                hinh = c;
+            }
+        }
+        public frmDangNhap()
+        {
+            InitializeComponent();
+        }
+                                                                                      
+        private void frmDangNhap_Load(object sender, EventArgs e)
+        {
+            //string OAuthURL = @"https://www.facebook.com/dialog/oauth?client_id=368621536821121
+            //                &redirect_uri=https://www.facebook.com/connect/login_success.html
+            //                &response_type=token
+            //                @scope=use_group,user_status,user_photos,";
+            //WebFacebook.Navigate(OAuthURL);
+            var access_token = collection["tk"];
+            FacebookClient fb = new FacebookClient(access_token);
+            dynamic info = fb.Get("me?fields=id,name,picture");
+            TaiKhoan taikhoan = new TaiKhoan(info.name, info.id, info.picture.data.url);
+            Session["access_token"] = access_token;
+            Session["taikhoan"] = taikhoan;
+        }
+    }
+}
